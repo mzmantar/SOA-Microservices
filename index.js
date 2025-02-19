c9bab4275b1e080a4fda5524526ecbfb08594092
MOHAMED-MEHDI ZMANTAR <155210616+mzmantar@users.noreply.github.com>
2025-02-19 23:26:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const axios = require("axios");
+const request = require("request");
 
-const API_KEY = "API_KEY";
+const API_KEY = "votre_clé_api";
 const CITY = "Sousse";
 const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=${CITY}&units=metric&lang=fr`;
 
-async function getWeatherData() {
-  try {
-    const response = await axios.get(BASE_URL);
-    const data = response.data;
-
-    console.log(`Météo à ${CITY} :`);
-    console.log(`Description : ${data.weather[0].description}`);
-    console.log(`empérature : ${data.main.temp}°C`);
-    console.log(`Humidité : ${data.main.humidity}%`);
-  } catch (error) {
-    console.error("Erreur :", error);
-  }
+function getWeatherData(callback) {
+    request(BASE_URL, function (error, response, body) {
+        if (error) {
+            callback(error, null);
+        } else {
+            const weatherData = JSON.parse(body);
+            callback(null, weatherData);
+        }
+    });
 }
 
-getWeatherData();
+getWeatherData((error, data) => {
+    if (error) {
+        console.error("Erreur :", error);
+    } else {
+        console.log(`Météo à ${CITY} :`);
+        console.log(`Description : ${data.weather[0].description}`);
+        console.log(`Température : ${data.main.temp}°C`);
+        console.log(`Humidité : ${data.main.humidity}%`);
+    }
+});
+
